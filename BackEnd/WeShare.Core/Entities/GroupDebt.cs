e8e3459f651a122d218101526645a277d03118a8
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-26 12:51:55 +0700

    Setup Core Layer

---
+++ BackEnd/WeShare.Core/Entities/GroupDebt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeShare.Core.Entities
+{
+    public class GroupDebt
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Updated_At { get; set; }
+    }
+}
---
+++ BackEnd/WeShare.Core/Enums/TaskStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeShare.Core.Enums
+{
+    public enum TaskStatusEnum
+    {
+        Created,
+        InProgress,
+        Done,
+        Canceled,
+    }
+}
