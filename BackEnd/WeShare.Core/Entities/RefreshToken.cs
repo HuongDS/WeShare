da3555051d013b6eda1545fb4ffdc5295572302f
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-27 22:43:36 +0700

    add refreshTokens table

---
+++ BackEnd/WeShare.Core/Entities/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeShare.Core.Interfaces;
+
+namespace WeShare.Core.Entities
+{
+    public class RefreshToken : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public string JwtId { get; set; }
+        public bool IsUsed { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
