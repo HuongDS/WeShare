3c7a0883610dcf9fe3f42d12f2d127a1cf7c0243
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-27 23:40:35 +0700

    add 2 method generic

--- BackEnd/WeShare.Core/Interfaces/IGenericRepository.cs
+++ BackEnd/WeShare.Core/Interfaces/IGenericRepository.cs
@@ -9,7 +9,9 @@ namespace WeShare.Core.Interfaces
     public interface IGenericRepository<T> where T : class, IEntity
     {
         Task AddAsync(T entity);
+        Task<int> CountAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate);
         void Delete(T entity);
+        Task<IEnumerable<T>> FindAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate);
         Task<IEnumerable<T>> GetAllAsyns();
         Task<T?> GetByIdAsync(int id);
         void Update(T entity);
--- BackEnd/WeShare.Infrastructure/Repositories/GenericRepository.cs
+++ BackEnd/WeShare.Infrastructure/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,10 @@ namespace WeShare.Infrastructure.Repositories
         {
             return await _dbSet.ToListAsync();
         }
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
         public async Task AddAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
@@ -42,6 +47,10 @@ namespace WeShare.Infrastructure.Repositories
                 _dbSet.Attach(entity);
             }
             _dbSet.Remove(entity);
+        }
+        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.CountAsync(predicate);
         }
     }
 }
